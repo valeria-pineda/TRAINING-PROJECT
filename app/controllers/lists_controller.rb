f4cd67ed7da006221d51a03a957bcad5e303b1c3
Valeria Pineda <valerializette.pinedacruz@kedgebs.com>
2022-07-05 18:40:00 +0200

    cludinary bug

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,6 +30,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :image_url)
+    params.require(:list).permit(:name, :image_url, :photo)
   end
 end
